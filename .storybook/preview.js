0101e25d8fed92a66fc250c8192352fe8f2bdb75
Erich Malberg <emalberg@live.com>
2024-06-30 23:21:46 -0600

    Added storybook and jest

---
+++ .storybook/preview.js
@@ -0,0 +1,18 @@
+/** @type { import('@storybook/react').Preview } */
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
